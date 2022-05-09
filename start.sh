dfb13c8052582d15003a31fa19b647c90d3988c3
bts420com <105052075+bts420com@users.noreply.github.com>
2022-05-10 03:22:21 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #bin #!bash
 
 ###### Adding Files ######
-wget -O /app/tobrot/aria2/dht.dat https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
-wget -O /app/tobrot/aria2/dht6.dat https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
-TRACKER=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt -: https://ngosang.github.io/trackerslist/trackers_all_http.txt -: https://newtrackon.com/api/all -: https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt -: https://torrends.to/torrent-tracker-list/?download=latest | awk '$1' | tr '\n' ',' | cat`
+wget -O /app/tobrot/aria2/dht.dat https://github.com/bts420com/bts420-leech/raw/main/asset-raw/dht.dat
+wget -O /app/tobrot/aria2/dht6.dat https://github.com/bts420com/bts420-leech/raw/main/asset-raw/dht6.dat
+TRACKER=`curl -Ns https://github.com/bts420com/bts420-leech/raw/main/asset-raw/all.txt -: https://github.com/bts420com/bts420-leech/raw/main/asset-raw/trackers_all_http.txt -: https://github.com/bts420com/bts420-leech/raw/main/asset-raw/all -: https://github.com/bts420com/bts420-leech/raw/main/asset-raw/AT_all.txt -: https://torrends.to/torrent-tracker-list/?download=latest | awk '$1' | tr '\n' ',' | cat`
 ran=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 ###### Done Addding Files ######
 
